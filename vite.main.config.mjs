d6b7740dedfc7ca37397d1b9a1326acd6087d3ab
danis <dsanjuan1999@gmail.com>
2025-12-18 22:28:13 -0300

    Importación de Twurple modificada y externalizada

--- vite.main.config.mjs
+++ vite.main.config.mjs
@@ -1,4 +1,16 @@
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            external: [
+                '@twurple/auth',
+                '@twurple/api',
+                '@twurple/chat',
+                '@twurple/eventsub-ws',
+                'node:fs/promises'
+            ],
+        },
+    },
+});
